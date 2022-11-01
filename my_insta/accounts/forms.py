37d5be78b175120611b8537a364e32b6544709bf
Mosunov Anton <antonmossunov@gmail.com>
2022-11-01 11:44:33 +0600

    Настройка маршрутизации функций пользователя.
    Настроена маршрутизация, созданы формы, для редактирования пользователя, смены пароля.

--- my_insta/accounts/forms.py
+++ my_insta/accounts/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 
@@ -26,32 +25,68 @@ class CustomUserCreationForm(forms.ModelForm):
 
     )
 
-
     class Meta:
         model = get_user_model()
         fields = ('username',  'email', 'avatar', 'password', 'password_confirm',
                   'first_name', 'about_user', 'phone', 'gender')
 
-        def clean(self):
-            cleaned_data = super().clean()
-            password = cleaned_data.get('password')
-            password_confirm = cleaned_data.get('password_confirm')
-            if password and password_confirm and password != password_confirm:
-                raise ValidationError('Пароли не совпадают')
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
 
-        def save(self, commit=True):
-            user = super().save(commit=False)
-            user.set_password(self.cleaned_data.get('password'))
-            user.groups.add('user')
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
 
-            if commit:
-                user.save()
-            return user
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label='')
+
+
+class CommentForm(forms.Form):
+    comment = forms.CharField(max_length=3000, required=False, label='Комментарий',
+                              widget=forms.Textarea(attrs={'name': 'body', 'rows': 5, 'cols': 21}))
 
 
 class UserChangeForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ('first_name', 'last_name', 'email', 'avatar', 'birthday', 'first_name',
+        fields = ('username', 'first_name', 'last_name', 'email', 'avatar', 'birthday', 'first_name',
                   'last_name', 'about_user', 'phone', 'gender',)
         labels = {'first_name': 'Имя', 'last_name': 'Фамилия', 'email': 'Email'}
+
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(label="Новый пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Старый пароль", strip=False, widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Старый пароль неправильный!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ['password', 'password_confirm', 'old_password']
--- my_insta/accounts/urls.py
+++ my_insta/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
-from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView
+from accounts.views import LoginView, logout_view, RegisterView, ProfileView, UserChangeView, ProfileFollowView
+from accounts.views import UserPasswordChangeView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('profile/<int:pk>/change', UserChangeView.as_view(), name='change'),
     path('logout/', logout_view, name='logout'),
     path('profile/<int:pk>/change', UserChangeView.as_view(), name='change'),
+    path('profile/<int:pk>/follow/', ProfileFollowView.as_view(), name='follow'),
+    path('<int:pk>/password_change', UserPasswordChangeView.as_view(), name='password_change')
 ]
